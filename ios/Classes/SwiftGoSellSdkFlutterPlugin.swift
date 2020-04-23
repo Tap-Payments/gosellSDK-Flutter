c5f2d7388dd1e82151c42ceadfebd1e14af371f5
Kareem Ahmed <k_m52@hotmail.com>
2020-04-23 16:47:49 +0300

    Added success / failure results to be passed to flutter.

--- ios/Classes/SwiftGoSellSdkFlutterPlugin.swift
+++ ios/Classes/SwiftGoSellSdkFlutterPlugin.swift
@@ -1,11 +1,13 @@
 import Flutter
 import UIKit
 import goSellSDK
+import TapCardValidator
 
 public class SwiftGoSellSdkFlutterPlugin: NSObject, FlutterPlugin {
     let session = Session()
     public var argsSessionParameters:[String:Any]?
     public var argsAppCredentials:[String:String]?
+    var flutterResult: FlutterResult?
     var argsDataSource:[String:Any]?{
       didSet{
         argsSessionParameters = argsDataSource?["sessionParameters"] as? [String : Any]
@@ -34,10 +36,12 @@ public class SwiftGoSellSdkFlutterPlugin: NSObject, FlutterPlugin {
     GoSellSDK.secretKey = secretKey
     GoSellSDK.mode = sdkMode
     GoSellSDK.language = sdkLang
+    session.delegate = self
     session.dataSource = self
     session.start()
-
-    result(["key": "iOS  + sssssssssss "])
+    flutterResult = result
+//    result(["key": "iOS  + sssssssssss "])
+    
   }
 }
 
@@ -312,5 +316,268 @@ extension SwiftGoSellSdkFlutterPlugin: SessionDataSource {
         }
       }
       return [CardType(cardType: .Debit), CardType(cardType: .Credit)]
+    }
+}
+
+extension SwiftGoSellSdkFlutterPlugin: SessionDelegate {
+    public func paymentSucceed(_ charge: Charge, on session: SessionProtocol) {
+      print(charge)
+        
+        var resultMap = [String: Any]()
+        resultMap["status"] = charge.status.textValue
+        resultMap["charge_id"] = charge.identifier
+        resultMap["description"] = charge.description
+        resultMap["message"] = charge.response?.message
+        
+        if let card = charge.card {
+            resultMap["card_first_six"] = card.firstSixDigits
+            resultMap["card_last_four"] = card.lastFourDigits
+            resultMap["card_object"] = card.object
+//            let cardBrand = CardBrand(rawValue: card.brand.rawValue)
+            resultMap["card_brand"] = card.brand.textValue
+            resultMap["card_exp_month"] = card.expirationMonth
+            resultMap["card_exp_year"] = card.expirationYear
+        }
+        
+        if let acquirer = charge.acquirer {
+            if let response = acquirer.response {
+                resultMap["acquirer_id"] = ""
+                resultMap["acquirer_response_code"] = response.code
+                resultMap["acquirer_response_message"] = response.message
+            }
+            
+        }
+        
+        resultMap["source_id"] = charge.source.identifier
+        resultMap["source_channel"] = charge.source.channel.textValue
+        resultMap["source_object"] = charge.source.object.textValue
+        resultMap["source_payment_type"] = charge.source.paymentType.textValue
+        
+        resultMap["sdk_result"] = "SUCCESS"
+        resultMap["trx_mode"] = "CHARGE"
+        
+        //pendingResult.success(resultMap);
+        if let flutterResult = flutterResult {
+            flutterResult(resultMap)
+        }
+    }
+    
+    public func paymentFailed(with charge: Charge?, error: TapSDKError?, on session: SessionProtocol) {
+                
+        var resultMap = [String: Any]()
+        if let charge = charge {
+            resultMap["status"] = charge.status.textValue
+            resultMap["charge_id"] = charge.identifier
+            resultMap["description"] = charge.description
+            resultMap["message"] = charge.response?.message
+            
+            if let card = charge.card {
+                resultMap["card_first_six"] = card.firstSixDigits
+                resultMap["card_last_four"] = card.lastFourDigits
+                resultMap["card_object"] = card.object
+                resultMap["card_brand"] = card.brand.textValue
+                resultMap["card_exp_month"] = card.expirationMonth
+                resultMap["card_exp_year"] = card.expirationYear
+            }
+            
+            if let acquirer = charge.acquirer {
+                if let response = acquirer.response {
+                    resultMap["acquirer_id"] = ""
+                    resultMap["acquirer_response_code"] = response.code
+                    resultMap["acquirer_response_message"] = response.message
+                }
+                
+            }
+            
+            resultMap["source_id"] = charge.source.identifier
+            resultMap["source_channel"] = charge.source.channel.textValue
+            resultMap["source_object"] = charge.source.object.textValue
+            resultMap["source_payment_type"] = charge.source.paymentType.textValue
+        }
+                
+        resultMap["sdk_result"] = "FAILED"
+        resultMap["trx_mode"] = "CHARGE"
+         
+        if let flutterResult = flutterResult {
+            flutterResult(resultMap)
+        }
+    }
+    
+    
+    
+    public func cardTokenized(_ token: Token, on session: SessionProtocol, customerRequestedToSaveTheCard saveCard: Bool) {
+       var resultMap:[String:Any] = [:]
+          resultMap["token"] = token.identifier
+          if let tokenDataSource = session.dataSource,
+            let tokenCurrency:Currency = tokenDataSource.currency as? Currency {
+            resultMap["token_currency"] = tokenCurrency.isoCode
+          }
+          resultMap["card_first_six"] = token.card.binNumber
+          resultMap["card_last_four"] = token.card.lastFourDigits
+          resultMap["card_object"] = token.card.object
+          resultMap["card_exp_month"] = token.card.expirationMonth
+          resultMap["card_exp_year"] = token.card.expirationYear
+          resultMap["sdk_result"] = "SUCCESS"
+          resultMap["trx_mode"] = "TOKENIZE"
+//          result.success(resultMap)
+        if let flutterResult = flutterResult {
+            flutterResult(resultMap)
+        }
+        
+    }
+    public func cardTokenizationFailed(with error: TapSDKError, on session: SessionProtocol) {
+      var resultMap:[String:Any] = [:]
+      resultMap["sdk_result"] = "SDK_ERROR"
+      resultMap["sdk_error_code"] = error.type
+      resultMap["sdk_error_message"] = error.description
+      resultMap["sdk_error_description"] = error.description
+//      result.success(resultMap)
+        if let flutterResult = flutterResult {
+            flutterResult(resultMap)
+        }
+    }
+}
+
+
+extension ChargeStatus {
+    var textValue: String {
+        switch self {
+        case .initiated:    return "INITIATED"
+        case .inProgress:   return "IN_PROGRESS"
+        case .abandoned:    return "ABANDONED"
+        case .cancelled:    return "CANCELLED"
+        case .failed:       return "FAILED"
+        case .declined:     return "DECLINED"
+        case .restricted:   return "RESTRICTED"
+        case .captured:     return "CAPTURED"
+        case .authorized:   return "AUTHORIZED"
+        case .unknown:        return "UNKNOWN"
+        case .void:         return "VOID"
+        }
+    }
+}
+
+extension SourceChannel {
+    var textValue: String {
+        switch self {
+        case .callCentre:       return "CALL_CENTRE"
+        case .internet:         return "INTERNET"
+        case .mailOrder:        return "MAIL_ORDER"
+        case .moto:             return "MOTO"
+        case .telephoneOrder:   return "TELEPHONE_ORDER"
+        case .voiceResponse:    return "VOICE_RESPONSE"
+        case .null:             return "null"
+        }
+    }
+}
+
+extension SourceObject {
+    var textValue: String {
+        switch self {
+            
+        case .token:    return "TOKEN"
+        case .source:   return "SOURCE"
+            
+        }
+    }
+}
+
+extension SourcePaymentType {
+    fileprivate struct RawValues {
+        
+        fileprivate static let table: [SourcePaymentType: [String]] = [
+        
+            .debitCard:        RawValues.debitCard,
+            .creditCard:    RawValues.creditCard,
+            .prepaidCard:    RawValues.prepaidCard,
+            .prepaidWallet:    RawValues.prepaidWallet,
+            .null:            RawValues.null
+        ]
+        
+        private static let debitCard        = ["DEBIT_CARD",        "DEBIT"]
+        private static let creditCard        = ["CREDIT_CARD",        "CREDIT"]
+        private static let prepaidCard        = ["PREPAID_CARD",        "PREPAID"]
+        private static let prepaidWallet    = ["PREPAID_WALLET",    "WALLET"]
+        private static let null                = ["null"]
+        
+        @available(*, unavailable) private init() {}
+    }
+    
+    var textValue: String {
+        return RawValues.table[self]!.first!
+    }
+}
+
+extension CardBrand {
+    var textValue: String {
+        return RawValues.table[self]?.first ?? ""
+    }
+    
+    fileprivate struct RawValues {
+
+        fileprivate static let table: [CardBrand: [String]] = [
+
+            .aiywaLoyalty       : RawValues.aiywaLoyalty,
+            .americanExpress    : RawValues.americanExpress,
+            .benefit            : RawValues.benefit,
+            .cardGuard          : RawValues.cardGuard,
+            .cbk                : RawValues.cbk,
+            .dankort            : RawValues.dankort,
+            .discover           : RawValues.discover,
+            .dinersClub         : RawValues.dinersClub,
+            .fawry              : RawValues.fawry,
+            .instaPayment       : RawValues.instaPayment,
+            .interPayment       : RawValues.interPayment,
+            .jcb                : RawValues.jcb,
+            .knet               : RawValues.knet,
+            .mada               : RawValues.mada,
+            .maestro            : RawValues.maestro,
+            .masterCard         : RawValues.masterCard,
+            .naps               : RawValues.naps,
+            .nspkMir            : RawValues.nspkMir,
+            .omanNet            : RawValues.omanNet,
+            .sadad              : RawValues.sadad,
+            .tap                : RawValues.tap,
+            .uatp               : RawValues.uatp,
+            .unionPay           : RawValues.unionPay,
+            .verve              : RawValues.verve,
+            .visa               : RawValues.visa,
+            .visaElectron        : RawValues.visaElectron,
+            .viva               : RawValues.viva,
+            .wataniya           : RawValues.wataniya,
+            .zain               : RawValues.zain
+        ]
+
+        private static let aiywaLoyalty     = ["Aiywa Loyalty"]
+        private static let americanExpress  = ["AMERICAN_EXPRESS", "AMEX"]
+        private static let benefit          = ["BENEFIT"]
+        private static let cardGuard        = ["CARDGUARD"]
+        private static let cbk              = ["CBK"]
+        private static let dankort          = ["DANKORT"]
+        private static let discover         = ["DISCOVER"]
+        private static let dinersClub       = ["DINERS_CLUB", "DINERS"]
+        private static let fawry            = ["FAWRY"]
+        private static let instaPayment     = ["INSTAPAY"]
+        private static let interPayment     = ["INTERPAY"]
+        private static let jcb              = ["JCB"]
+        private static let knet             = ["KNET"]
+        private static let mada             = ["MADA"]
+        private static let maestro          = ["MAESTRO"]
+        private static let masterCard       = ["MASTERCARD"]
+        private static let naps             = ["NAPS"]
+        private static let nspkMir          = ["NSPK"]
+        private static let omanNet          = ["OMAN_NET"]
+        private static let sadad            = ["SADAD_ACCOUNT"]
+        private static let tap              = ["TAP"]
+        private static let uatp             = ["UATP"]
+        private static let unionPay         = ["UNION_PAY", "UNIONPAY"]
+        private static let verve            = ["VERVE"]
+        private static let visa             = ["VISA"]
+        private static let visaElectron        = ["VISA_ELECTRON"]
+        private static let viva             = ["Viva PAY"]
+        private static let wataniya         = ["Wataniya PAY"]
+        private static let zain             = ["Zain PAY"]
+
+        @available(*, unavailable) private init() {}
     }
 }
