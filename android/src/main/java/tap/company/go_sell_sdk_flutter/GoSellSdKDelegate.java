74c27e025548f138eb53f1c12316ee5b15a8a3b0
Raza Shabbir <razashabbir1998@gmail.com>
2024-01-04 14:29:19 +0500

    big decimal change

--- android/src/main/java/tap/company/go_sell_sdk_flutter/GoSellSdKDelegate.java
+++ android/src/main/java/tap/company/go_sell_sdk_flutter/GoSellSdKDelegate.java
@@ -192,7 +192,7 @@ public class GoSellSdKDelegate implements PluginRegistry.ActivityResultListener,
                     + (String) Objects.requireNonNull(sessionParameters.get("amount")));
             amount = BigDecimal.ZERO;
         }
-        sdkSession.setAmount(0); // ** Required **
+        sdkSession.setAmount(BigDecimal.ZERO); // ** Required **
 
         sdkSession.setPaymentItems(DeserializationUtil.getPaymentItems(sessionParameters.get("paymentitems")));// **
         // Optional
