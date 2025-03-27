903551bd0e30a8f64585c5f3298f3125dbff546e
MultiX0 <ishiiro1990@gmail.com>
2025-03-27 06:22:12 +0300

    fix issue 3

--- android/src/main/java/tap/company/go_sell_sdk_flutter/GoSellSdkFlutterPlugin.java
+++ android/src/main/java/tap/company/go_sell_sdk_flutter/GoSellSdkFlutterPlugin.java
@@ -22,58 +22,22 @@ import io.flutter.plugin.common.MethodChannel;
 
 public class GoSellSdkFlutterPlugin implements MethodChannel.MethodCallHandler, FlutterPlugin, ActivityAware {
 
-    private class LifeCycleObserver
+    private MethodChannel channel;
+    private GoSellSdKDelegate delegate;
+    private FlutterPluginBinding pluginBinding;
+    private ActivityPluginBinding activityBinding;
+    private Application application;
+    private Activity activity;
+    private Lifecycle lifecycle;
+    private LifeCycleObserver observer;
+    private static final String CHANNEL = "go_sell_sdk_flutter";
+
+    private class LifeCycleObserver 
             implements Application.ActivityLifecycleCallbacks, DefaultLifecycleObserver {
         private final Activity thisActivity;
 
         LifeCycleObserver(Activity activity) {
             this.thisActivity = activity;
-        }
-
-        @Override
-        public void onCreate(@NonNull LifecycleOwner owner) {
-        }
-
-        @Override
-        public void onStart(@NonNull LifecycleOwner owner) {
-        }
-
-        @Override
-        public void onResume(@NonNull LifecycleOwner owner) {
-        }
-
-        @Override
-        public void onPause(@NonNull LifecycleOwner owner) {
-        }
-
-        @Override
-        public void onStop(@NonNull LifecycleOwner owner) {
-            onActivityStopped(thisActivity);
-        }
-
-        @Override
-        public void onDestroy(@NonNull LifecycleOwner owner) {
-            onActivityDestroyed(thisActivity);
-        }
-
-        @Override
-        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-        }
-
-        @Override
-        public void onActivityStarted(Activity activity) {
-        }
-
-        @Override
-        public void onActivityResumed(Activity activity) {
-        }
-
-        @Override
-        public void onActivityPaused(Activity activity) {
-        }
-
-        @Override
-        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
         }
 
         @Override
@@ -84,47 +48,62 @@ public class GoSellSdkFlutterPlugin implements MethodChannel.MethodCallHandler, 
             }
         }
 
+        // Other required method stubs... (keep the previous implementation)
         @Override
-        public void onActivityStopped(Activity activity) {
-            if (thisActivity == activity) {
-            }
+        public void onCreate(@NonNull LifecycleOwner owner) {}
+        @Override
+        public void onStart(@NonNull LifecycleOwner owner) {}
+        @Override
+        public void onResume(@NonNull LifecycleOwner owner) {}
+        @Override
+        public void onPause(@NonNull LifecycleOwner owner) {}
+        @Override
+        public void onStop(@NonNull LifecycleOwner owner) {}
+        @Override
+        public void onDestroy(@NonNull LifecycleOwner owner) {}
+        @Override
+        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {}
+        @Override
+        public void onActivityStarted(Activity activity) {}
+        @Override
+        public void onActivityResumed(Activity activity) {}
+        @Override
+        public void onActivityPaused(Activity activity) {}
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
+        @Override
+        public void onActivityStopped(Activity activity) {}
+    }
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        pluginBinding = flutterPluginBinding;
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), CHANNEL);
+        channel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        if (channel != null) {
+            channel.setMethodCallHandler(null);
+            channel = null;
         }
-    }
-
-    private MethodChannel channel;
-    private GoSellSdKDelegate delegate;
-    private FlutterPluginBinding pluginBinding;
-    private ActivityPluginBinding activityBinding;
-    private Application application;
-    private Activity activity;
-    private Lifecycle lifecycle;
-    private LifeCycleObserver observer;
-    private static final String CHANNEL = "tap.company.go_sell_sdk_flutter.GoSellSdkFlutterPlugin";
-
-    @Override
-    public void onAttachedToEngine(FlutterPluginBinding binding) {
-        pluginBinding = binding;
-    }
-
-    @Override
-    public void onDetachedFromEngine(FlutterPluginBinding binding) {
         pluginBinding = null;
     }
 
     @Override
-    public void onAttachedToActivity(ActivityPluginBinding binding) {
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
         activityBinding = binding;
-        setup(
-                pluginBinding.getBinaryMessenger(),
-                (Application) pluginBinding.getApplicationContext(),
-                binding.getActivity(),
-                binding
-        );
-    }
-
-    @Override
-    public void onDetachedFromActivity() {
-        tearDown();
+        activity = binding.getActivity();
+        application = (Application) activity.getApplicationContext();
+        
+        delegate = new GoSellSdKDelegate(activity);
+        
+        binding.addActivityResultListener(delegate);
+        binding.addRequestPermissionsResultListener(delegate);
+        
+        observer = new LifeCycleObserver(activity);
+        application.registerActivityLifecycleCallbacks(observer);
     }
 
     @Override
@@ -133,103 +112,68 @@ public class GoSellSdkFlutterPlugin implements MethodChannel.MethodCallHandler, 
     }
 
     @Override
-    public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
         onAttachedToActivity(binding);
     }
 
-    private void setup(
-            final BinaryMessenger messenger,
-            final Application application,
-            final Activity activity,
-            final ActivityPluginBinding activityBinding) {
-        this.activity = activity;
-        this.application = application;
-        this.delegate = constructDelegate(activity);
-        channel = new MethodChannel(messenger, "go_sell_sdk_flutter");
-        channel.setMethodCallHandler(this);
-        observer = new LifeCycleObserver(activity);
-
-        if (activityBinding != null) {
-            activityBinding.addActivityResultListener(delegate);
-            activityBinding.addRequestPermissionsResultListener(delegate);
-        }
-    }
-
-    private void tearDown() {
+    @Override
+    public void onDetachedFromActivity() {
         if (activityBinding != null) {
             activityBinding.removeActivityResultListener(delegate);
             activityBinding.removeRequestPermissionsResultListener(delegate);
             activityBinding = null;
         }
         
-        if (lifecycle != null) {
-            lifecycle.removeObserver(observer);
-            lifecycle = null;
-        }
-        
-        delegate = null;
-        
-        if (channel != null) {
-            channel.setMethodCallHandler(null);
-            channel = null;
-        }
-        
-        if (application != null) {
+        if (application != null && observer != null) {
             application.unregisterActivityLifecycleCallbacks(observer);
-            application = null;
         }
-    }
-
-    private final GoSellSdKDelegate constructDelegate(final Activity setupActivity) {
-        return new GoSellSdKDelegate(setupActivity);
-    }
-
-    private static class MethodResultWrapper implements MethodChannel.Result {
-        private MethodChannel.Result methodResult;
-        private Handler handler;
-
-        MethodResultWrapper(MethodChannel.Result result) {
-            methodResult = result;
-            handler = new Handler(Looper.getMainLooper());
-        }
-
-        @Override
-        public void success(final Object result) {
-            System.out.println("success coming from delegate : " + result);
-            handler.post(() -> methodResult.success(result));
-        }
-
-        @Override
-        public void error(
-                final String errorCode, final String errorMessage, final Object errorDetails) {
-            System.out.println("error encountered................." + errorCode);
-            handler.post(() -> methodResult.error(errorCode, errorMessage, errorDetails));
-        }
-
-        @Override
-        public void notImplemented() {
-            handler.post(() -> methodResult.notImplemented());
-        }
+        
+        activity = null;
+        delegate = null;
+        observer = null;
     }
 
     @Override
-    public void onMethodCall(MethodCall call, MethodChannel.Result rawResult) {
-        HashMap<String, Object> args = call.arguments();
-        System.out.println("args : " + args);
-        System.out.println("onMethodCall..... started");
+    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result rawResult) {
+        MethodChannel.Result result = new MethodResultWrapper(rawResult);
         
         if (activity == null) {
             rawResult.error("no_activity", "SDK plugin requires a foreground activity.", null);
             return;
         }
 
-        if (call.method.equals("terminate_session")) {
-            System.out.println("terminate session!");
+        HashMap<String, Object> args = call.arguments();
+        
+        if ("terminate_session".equals(call.method)) {
             delegate.terminateSDKSession();
             return;
         }
         
-        MethodChannel.Result result = new MethodResultWrapper(rawResult);
         delegate.startSDK(call, result, args);
+    }
+
+    private static class MethodResultWrapper implements MethodChannel.Result {
+        private final MethodChannel.Result methodResult;
+        private final Handler handler;
+
+        MethodResultWrapper(MethodChannel.Result result) {
+            this.methodResult = result;
+            this.handler = new Handler(Looper.getMainLooper());
+        }
+
+        @Override
+        public void success(final Object result) {
+            handler.post(() -> methodResult.success(result));
+        }
+
+        @Override
+        public void error(String errorCode, String errorMessage, Object errorDetails) {
+            handler.post(() -> methodResult.error(errorCode, errorMessage, errorDetails));
+        }
+
+        @Override
+        public void notImplemented() {
+            handler.post(methodResult::notImplemented);
+        }
     }
 }
